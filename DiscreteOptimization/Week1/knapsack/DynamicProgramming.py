c2174b191128e326124805c3eb7646a0964b5816
Bidit Devkota <devkotabidit@gmail.com>
2018-05-04 21:07:15 +0545

    dynamic programming added

---
+++ DiscreteOptimization/Week1/knapsack/DynamicProgramming.py
@@ -0,0 +1,54 @@
+import sys
+
+def dynamic_programming(k, n, weight, value, memoized_item):
+
+    if (n <= 0):
+        return 0
+
+    w = weight[n]
+    v = value[n]
+
+    try:
+        previous_value = memoized_item[(k, n)]
+    except KeyError:
+        previous_value = dynamic_programming(k, n-1, weight, value, memoized_item)
+
+    if (w  <= k):
+        return_value = max(previous_value, v  + dynamic_programming(k-w, n-1, weight, value, memoized_item))
+        memoized_item[(k,n)] = return_value
+        return return_value
+    return previous_value
+
+
+def main():
+    f = open(sys.argv[1], 'r')
+    N, K = map(lambda x: int(x), f.readline().lstrip().rstrip().split(" "))
+    value, weight = list(), list()
+    weight.append(0)
+    value.append(0)
+
+    for line in f.readlines():
+        v, w = map(lambda x: int(x), line.lstrip().rstrip().split(" "))
+
+        weight.append(w)
+        value.append(v)
+
+    dynamic_table = [[0]*(N+1)]*(K+1)
+
+    memoized_item = dict()
+
+    for n in range(0, N+1):
+        for k in range(0, K+1):
+
+            dynamic_table[k][n] = dynamic_programming(k, n, weight, value, memoized_item)
+
+
+    #for l in dynamic_table:
+        #print(l)
+    print(dynamic_table[K][N])
+
+if __name__ == '__main__':
+    main()
+
+
+
